b96af9ee8de6848599448eafff0d7aa7a4dc0eac
seanpeak-dev <seanpeak.dev@gmail.com>
2021-10-21 15:26:11 +0900

    Feat: InfiniteScroll

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -7,9 +7,10 @@ const setMsgs = (data) => writeDB('messages', data);
 const messagesRoute = [{
     method: "get",
     route: '/messages',
-    handler: (req, res) => {
+    handler: ({ query: { cursor = ''}}, res) => {
         const msgs = getMsgs();
-        res.send(msgs);
+        const fromIdx = msgs.findIndex((msg) => msg.id === cursor) + 1;
+        res.send(msgs.slice(fromIdx, fromIdx + 10));
     }
 }, {
     method: "get",
